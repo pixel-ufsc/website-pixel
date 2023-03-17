dd45cc600216feef018c3f3e47dee7a4ac3ee03c
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-03-17 20:02:31 -0300

    Criado componente 'HeaderButton'

---
+++ next-frontend/src/components/ui/header/header-button/header-button.component.jsx
@@ -0,0 +1,17 @@
+import styles from './header-button.module.css';
+import Link from 'next/link';
+import P from 'prop-types';
+
+export default function HeaderButton({ children, href, selected, ...props }) {
+    return (
+        <Link href={href} className={selected ? `${styles.button} ${styles.selected}` : styles.button} {...props}>
+            {children}
+        </Link>
+    );
+}
+
+HeaderButton.propTypes = {
+    children: P.node.isRequired,
+    href: P.string,
+    selected: P.bool,
+};
