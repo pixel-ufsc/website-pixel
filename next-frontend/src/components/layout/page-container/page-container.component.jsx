8c68d5bf9d99b3b0a30c03e98e2c590279f677ec
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-03-17 19:10:59 -0300

    Criado componente 'PageContainer'

---
+++ next-frontend/src/components/layout/page-container/page-container.component.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+
+// Componente usado para envolver as páginas, evitando a repetição do código de Head das páginas e
+// garatindo um fallback padrão para o título e descrição das páginas.
+
+const fallback = {
+    title: 'Pixel - Soluções Digitais',
+    description:
+        'Desenvolvendo soluções digitais que fazem a diferença. A Pixel é a empresa júnior de Ciências da Computação e de Sistemas de Informação da Universidade Federal de Santa Catarina (UFSC), sediada em Florianópolis.',
+};
+
+export default function PageContainer({ title, description, children }) {
+    return (
+        <>
+            <Head>
+                <title>{title ? title : fallback.title}</title>
+                <meta name="description" content={description ? description : fallback.description} />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>{children}</main>
+        </>
+    );
+}
