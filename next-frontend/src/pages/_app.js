d03f1d03fd0a4a32baaa9d2ca09cbcd7225375a7
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-03-17 19:04:53 -0300

    Removido código de template do NextJS e aplicado Prettier nos arquivos

--- next-frontend/src/pages/_app.js
+++ next-frontend/src/pages/_app.js
@@ -1,5 +1,5 @@
-import '@/src/components/styles/globals.css'
+import '@/src/components/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
