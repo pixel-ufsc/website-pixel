fde23cfe63533583c0d80272b27e3176cd9f881f
talisbreda <talis.breda@outlook.com>
2023-03-20 20:35:09 -0300

    Add card component

--- next-frontend/src/pages/index.js
+++ next-frontend/src/pages/index.js
@@ -1,10 +1,12 @@
 // Components
 import PageContainer from '@components/layout/page-container/page-container.component';
+import Card from '@components/ui/card/card.component';
 
 export default function Home() {
     return (
         <PageContainer>
             <h1>Home</h1>
+            <Card imageSrc='/talis.jpg' name='Tális Breda' role="Desenvolvedor"/>
         </PageContainer>
     );
 }
