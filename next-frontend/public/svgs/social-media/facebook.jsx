faee087b5338d1c0b66c09d1f7658cfd6c4c2447
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-03-22 14:33:03 -0300

    LinkedIn, WhatsApp icons and responsive container

--- next-frontend/public/svgs/social-media/facebook.jsx
+++ next-frontend/public/svgs/social-media/facebook.jsx
@@ -1,6 +1,6 @@
 export default function FacebookIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFF">
             <path d="M3 12.9C3 12.9 3.1 12.9 3 12.9 3.1 12.9 3 12.9 3 12.9zM12 2C6.5 2 2 6.5 2 12c0 5 3.7 9.2 8.6 9.9v-7.5H8.4V12h2.3v-1.6c0-2.6 1.2-3.7 3.4-3.7 1 0 1.5.1 1.8.1v2h-1.5c-.9 0-1.2.9-1.2 1.9V12h2.7l-.4 2.4h-2.3v7.5c4.9-.6 8.8-4.8 8.8-9.9C22 6.5 17.5 2 12 2z" />
         </svg>
     );
