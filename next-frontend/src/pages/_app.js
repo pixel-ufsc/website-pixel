48a2de75d3d5e47be117401890962c77ad1a193d
Matheus Antonio <17206723+hknmtt@users.noreply.github.com>
2023-03-23 22:48:53 -0300

    Adicionado switch on/off de renderizador de header

--- next-frontend/src/pages/_app.js
+++ next-frontend/src/pages/_app.js
@@ -1,14 +1,33 @@
 import '@styles/globals.css';
+import { useRouter } from 'next/router';
 // Components
 import Header from '@components/ui/header/header/header.component';
 import MediaHeader from '@components/ui/header/media-header/media-header.component';
 
 export default function App({ Component, pageProps }) {
-    return (
-        <>
-            <MediaHeader />
-            <Header />
-            <Component {...pageProps} />
-        </>
-    );
+    const router = useRouter();
+    // Caso o path seja /links-uteis, não renderiza o Header
+    // Adicione mais casos para caso a página não for renderizar o Header
+    switch (router.pathname) {
+        case '/links-uteis':
+            return <Component {...pageProps} />;
+
+        default:
+            return (
+                <>
+                    <MediaHeader />
+                    <Header />
+                    <Component {...pageProps} />
+                </>
+            );
+    }
+
+    // return (
+    //     <>
+    //         <MediaHeader />
+    //         <Header />
+    //         <Component {...pageProps} />
+    //     </>
+    // );
 }
+
---
+++ next-frontend/src/pages/links-uteis/index.jsx
@@ -0,0 +1,15 @@
+// Components
+import PageContainer from '@components/layout/page-container/page-container.component';
+import WhatsappButton from '@components/ui/buttons/whatsapp-button/whatsapp-button.component';
+
+export default function LinksUteis() {
+    return (
+        <>
+            <PageContainer>
+                <h1>Portfolio</h1>
+            </PageContainer>
+            <WhatsappButton />
+        </>
+    );
+}
+
