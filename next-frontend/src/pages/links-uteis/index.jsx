36dd0cb470b310dcf1c3302ededde364f77747a6
Matheus Antonio <17206723+hknmtt@users.noreply.github.com>
2023-03-24 00:02:24 -0300

    Adicionado background e botões

--- next-frontend/src/pages/links-uteis/index.jsx
+++ next-frontend/src/pages/links-uteis/index.jsx
@@ -1,18 +1,37 @@
-// Assets
-import PixelLogoHorizontalBranca from '@public/svgs/pixel/pixel-logo-horizontal-branca';
+import styles from './links-uteis.module.css';
 // Components
 import PageContainer from '@components/layout/page-container/page-container.component';
 import WhatsappButton from '@components/ui/buttons/whatsapp-button/whatsapp-button.component';
+import LinksHeader from '@components/ui/header/links-header/links-header.component';
+import LinkButton from '@components/ui/buttons/link-button/link-button.component';
 
 export default function LinksUteis() {
     return (
-        <>
+        <div className={styles.background}>
             <PageContainer>
-                <PixelLogoHorizontalBranca />
-                <h1>Portfolio</h1>
+                <div className={styles.container}>
+                    <LinksHeader />
+                    <ul className={styles.container_buttons}>
+                        <il>
+                            <LinkButton texto="Nosso Site" link="/" />
+                        </il>
+                        <il>
+                            <LinkButton texto="LinkedIn" link="https://www.linkedin.com/company/ejpixel" />
+                        </il>
+                        <il>
+                            <LinkButton texto="Facebook" link="https://www.fb.com/ejpixel" />
+                        </il>
+                        <il>
+                            <LinkButton
+                                texto="E-book Gestão do conhecimento"
+                                link="https://drive.google.com/file/d/1dLGZcmuvDa9tutWsMEfMLOGMvdeUZjlI/view"
+                            />
+                        </il>
+                    </ul>
+                </div>
             </PageContainer>
             <WhatsappButton />
-        </>
+        </div>
     );
 }
 
