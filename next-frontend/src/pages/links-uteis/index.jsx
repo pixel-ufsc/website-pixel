e36d4e7e5dc5340a93141ab07e9fd761c60b76ab
Matheus Antonio <17206723+hknmtt@users.noreply.github.com>
2023-03-23 23:01:39 -0300

    Adicionado logo branca da pixel

--- next-frontend/src/pages/links-uteis/index.jsx
+++ next-frontend/src/pages/links-uteis/index.jsx
@@ -1,3 +1,5 @@
+// Assets
+import PixelLogoHorizontalBranca from '@public/svgs/pixel/pixel-logo-horizontal-branca';
 // Components
 import PageContainer from '@components/layout/page-container/page-container.component';
 import WhatsappButton from '@components/ui/buttons/whatsapp-button/whatsapp-button.component';
@@ -6,6 +8,7 @@ export default function LinksUteis() {
     return (
         <>
             <PageContainer>
+                <PixelLogoHorizontalBranca />
                 <h1>Portfolio</h1>
             </PageContainer>
             <WhatsappButton />
