8b63ada477f9b5b74f3cdb27bc8c6d664b82d0de
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-03-27 15:26:13 -0300

    adding termo-de-uso and politica-de-privacidade pages

---
+++ next-frontend/src/pages/politica-de-privacidade/index.jsx
@@ -0,0 +1,14 @@
+// Components
+import PageContainer from '@components/layout/page-container/page-container.component';
+import WhatsappButton from '@components/ui/buttons/whatsapp-button/whatsapp-button.component';
+
+export default function PoliticaPrivacidade() {
+    return (
+        <>
+            <PageContainer>
+                <h1>Política de Privacidade</h1>
+            </PageContainer>
+            <WhatsappButton />
+        </>
+    );
+}
