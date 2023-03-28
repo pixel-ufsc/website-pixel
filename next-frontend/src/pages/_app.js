2ab7fd7603e542d1bbb5cfd529f08d546cf31613
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-03-27 21:54:36 -0300

    Removed unused imports

--- next-frontend/src/pages/_app.js
+++ next-frontend/src/pages/_app.js
@@ -1,8 +1,4 @@
 import '@styles/globals.css';
-import { useRouter } from 'next/router';
-// Components
-import Header from '@components/ui/header/header/header.component';
-import MediaHeader from '@components/ui/header/media-header/media-header.component';
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
