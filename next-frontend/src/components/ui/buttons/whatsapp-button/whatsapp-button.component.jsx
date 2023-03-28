da7381ec31b93f9fe454238d465e25bac00b10cc
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-03-27 21:00:50 -0300

    Modified WhatsApp button animation

--- next-frontend/src/components/ui/buttons/whatsapp-button/whatsapp-button.component.jsx
+++ next-frontend/src/components/ui/buttons/whatsapp-button/whatsapp-button.component.jsx
@@ -7,25 +7,8 @@ import WhatsappIcon from '@public/svgs/social-media/whatsapp';
 import { useEffect, useState } from 'react';
 
 export default function WhatsappButton() {
-    const [isVisible, setIsVisible] = useState(false);
-
-    useEffect(() => {
-        const timeOut = setTimeout(() => {
-            setIsVisible(() => true);
-        }, 3000);
-
-        return () => {
-            setIsVisible(() => false);
-            clearTimeout(timeOut);
-        };
-    }, []);
-
     return (
-        <a
-            className={isVisible ? `${styles.button} ${styles.button_on_screen}` : `${styles.button}`}
-            href="https://wa.a/p069um"
-            target="_blank"
-        >
+        <a className={styles.button} href="https://wa.a/p069um" target="_blank">
             <WhatsappIcon className={styles.icon} />
         </a>
     );
