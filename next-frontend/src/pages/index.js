7cecfe2e8f354ad8c15078381754557a470ac121
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-04-09 14:53:34 -0300

    Removed content from home page

--- next-frontend/src/pages/index.js
+++ next-frontend/src/pages/index.js
@@ -7,24 +7,10 @@ import WhatsappButton from '@components/ui/buttons/whatsapp-button/whatsapp-butt
 import { getAllDirectors, getAllMembers } from '@lib/sanity.client';
 
 export default function Home({ members, directors }) {
-    console.log(members);
-    console.log(directors);
     return (
-        <>
-            <PageContainer>
-                <h1>Home</h1>
-                <MemberCard
-                    imageSrc={'https://ejpixel.com.br/wp-content/uploads/2022/11/Andre-280x280.jpg'}
-                    name={'Talis'}
-                    role={'Diretor de Comercial'}
-                />
-                <MemberCard />
-                <div style={{ margin: '20px 0', marginLeft: '5px' }}>
-                    <HoverableButton href={'/servicos'} label={'Conheça nossos serviços'} arrowed />
-                </div>
-            </PageContainer>
-            <WhatsappButton />
-        </>
+        <PageContainer>
+            <h1>Home</h1>
+        </PageContainer>
     );
 }
 
