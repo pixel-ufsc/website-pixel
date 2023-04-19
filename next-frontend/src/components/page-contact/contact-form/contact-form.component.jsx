9206b4824678f4e3b3ab7941963bfc8395d8f482
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-04-19 19:37:43 -0300

    Netlify forms integration attempt

--- next-frontend/src/components/page-contact/contact-form/contact-form.component.jsx
+++ next-frontend/src/components/page-contact/contact-form/contact-form.component.jsx
@@ -2,12 +2,14 @@ import styles from './contact-form.module.css';
 
 export default function ContactForm() {
     return (
-        <form className={styles.form}>
-            <input className={styles.input} type="text" placeholder="Nome completo" />
-            <input className={styles.input} type="text" placeholder="Seu e-mail" />
-            <input className={styles.input} type="text" placeholder="Número de telefone" />
-
-            <textarea className={styles.textarea} placeholder="Mensagem"></textarea>
+        <form className={styles.form} action="/sucesso" method="POST" data-netlify="true">
+            <input className={styles.input} type="text" placeholder="Nome completo" name="name" />
+            <input className={styles.input} type="text" placeholder="Seu e-mail" name="email" />
+            <input className={styles.input} type="text" placeholder="Número de telefone" name="phone" />
+            <textarea className={styles.textarea} placeholder="Mensagem" name="text-message" />
+            <button className={styles.button} type="submit">
+                Enviar
+            </button>
         </form>
     );
 }
