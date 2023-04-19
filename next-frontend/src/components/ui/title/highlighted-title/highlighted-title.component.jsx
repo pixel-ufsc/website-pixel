f2f21c15cdf074aef338cd203c24d6f90abefac6
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-04-18 21:01:37 -0300

    Created 'SimpleTitle' component

--- next-frontend/src/components/ui/title/highlighted-title/highlighted-title.component.jsx
+++ next-frontend/src/components/ui/title/highlighted-title/highlighted-title.component.jsx
@@ -1,6 +1,6 @@
 import styles from './highlighted-title.module.css';
 
-export default function HighlightedTitle({ children, className, fontSize = 'medium', ...props }) {
+export default function HighlightedTitle({ children, className, ...props }) {
     return (
         <h1 className={className ? `${styles.title} ${className}` : styles.title} {...props}>
             <span className={styles.text}>{children}</span>
