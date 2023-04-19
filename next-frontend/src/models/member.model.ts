4cc36b79c7891aeb216aa96b32845871fd578228
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-04-18 21:04:37 -0300

    Created 'Directors' component

---
+++ next-frontend/src/models/member.model.ts
@@ -0,0 +1,11 @@
+import { SanityDocument, Image } from '@sanity/types';
+
+export interface Member extends SanityDocument {
+    data: {
+        name: string;
+        role: string;
+        is_director: boolean;
+        // The image prop is set to be a Sanity Image Object. Make sure to handle the image object correctly in the component.
+        image: Image;
+    };
+}
