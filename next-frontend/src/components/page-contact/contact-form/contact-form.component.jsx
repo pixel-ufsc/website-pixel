16b8ff325381cf2b1d02ffc4bd1c12d7e3a333d1
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-04-24 20:13:08 -0300

    Hidden unfinished components

--- next-frontend/src/components/page-contact/contact-form/contact-form.component.jsx
+++ next-frontend/src/components/page-contact/contact-form/contact-form.component.jsx
@@ -1,4 +1,6 @@
 import styles from './contact-form.module.css';
+// Components
+import HoverableButton from '@components/ui/buttons/hoverable-button/hoverable-button.component';
 
 export default function ContactForm() {
     return (
@@ -7,9 +9,7 @@ export default function ContactForm() {
             <input className={styles.input} type="text" placeholder="Seu e-mail" name="email" />
             <input className={styles.input} type="text" placeholder="Número de telefone" name="phone" />
             <textarea className={styles.textarea} placeholder="Mensagem" name="text-message" />
-            <button className={styles.button} type="submit">
-                Enviar
-            </button>
+            <HoverableButton label="Enviar" type="submit" />
         </form>
     );
 }
--- next-frontend/src/pages/contato/index.jsx
+++ next-frontend/src/pages/contato/index.jsx
@@ -7,7 +7,7 @@ export default function Contato() {
     return (
         <PageContainer>
             <UnderMaintenance />
-            <ContactContainer />
+            {/* <ContactContainer /> */}
         </PageContainer>
     );
 }
