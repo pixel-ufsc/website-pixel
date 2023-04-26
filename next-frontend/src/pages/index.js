cd9d9bd07d44ba454b99ea67a6552a97db19fb65
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-04-26 18:43:04 -0300

    Added 'UnderMaintenance' component

--- next-frontend/src/pages/index.js
+++ next-frontend/src/pages/index.js
@@ -4,12 +4,14 @@ import { getAllDirectors, getAllSocialInfo } from '@lib/sanity.client';
 import PageContainer from '@components/layout/page-container/page-container.component';
 import Directors from '@components/page-home/directors-members/directors.component';
 import IntroContent from '@components/page-home/intro-content/intro-content.component';
+import UnderMaintenance from '@components/ui/under-maintenance/under-maintenance.component';
 
 export default function Home({ directorsData }) {
     return (
         <PageContainer>
             <IntroContent />
             <Directors directors={directorsData} />
+            <UnderMaintenance />
         </PageContainer>
     );
 }
