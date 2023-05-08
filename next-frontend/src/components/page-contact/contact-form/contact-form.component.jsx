1b5cecf630c851763f60669927e857f2cdd8023f
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-05-08 09:36:38 -0300

    Styled form element

--- next-frontend/src/components/page-contact/contact-form/contact-form.component.jsx
+++ next-frontend/src/components/page-contact/contact-form/contact-form.component.jsx
@@ -5,11 +5,34 @@ import HoverableButton from '@components/ui/buttons/hoverable-button/hoverable-b
 export default function ContactForm() {
     return (
         <form className={styles.form} action="/sucesso" method="POST" data-netlify="true">
-            <input className={styles.input} type="text" placeholder="Nome completo" name="name" />
-            <input className={styles.input} type="text" placeholder="Seu e-mail" name="email" />
-            <input className={styles.input} type="text" placeholder="Número de telefone" name="phone" />
-            <textarea className={styles.textarea} placeholder="Mensagem" name="text-message" />
-            <HoverableButton label="Enviar" type="submit" />
+            <div className={styles.form_element}>
+                <label className={styles.label} htmlFor="name">
+                    Nome completo
+                </label>
+                <input className={styles.input} type="text" placeholder="Insira seu nome completo" name="name" />
+            </div>
+
+            <div className={styles.form_element}>
+                <label className={styles.label} htmlFor="email">
+                    E-mail
+                </label>
+                <input className={styles.input} type="text" placeholder="exemplo@email.com" name="email" />
+            </div>
+
+            <div className={styles.form_element}>
+                <label className={styles.label} htmlFor="phone">
+                    Número de telefone
+                </label>
+                <input className={styles.input} type="text" placeholder="(99) 99999-9999" name="phone" />
+            </div>
+
+            <div className={styles.form_element}>
+                <label className={styles.label} htmlFor="text-message">
+                    Mensagem
+                </label>
+                <textarea className={styles.textarea} placeholder="Escreva aqui mensagem..." name="text-message" />
+            </div>
+            <HoverableButton label="Enviar mensagem" type="submit" arrowed={true} className={styles.submit_button} />
         </form>
     );
 }
