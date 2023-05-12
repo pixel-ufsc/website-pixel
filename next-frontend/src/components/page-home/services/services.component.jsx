6cc296666ab560f5905fb69b3cbffe3f468366db
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-05-12 11:39:28 -0300

    adding alt text to images of the services component in home page

--- next-frontend/src/components/page-home/services/services.component.jsx
+++ next-frontend/src/components/page-home/services/services.component.jsx
@@ -10,7 +10,12 @@ export default function Services() {
             <h2>Serviços</h2>
             <div>
                 <div className={styles.serviceCard}>
-                    <Image src={websiteServiceImage} width={106} height={86} alt="" />
+                    <Image
+                        src={websiteServiceImage}
+                        width={106}
+                        height={86}
+                        alt="Página web em um navegador tendo como conteúdo a sintaxe de um elemento HTML."
+                    />
                     <h3>Desenvolvimento de sites</h3>
                     <p>
                         Desenvolvimento de sistemas web e sites baseados nas mais novas tecnologias, com a preocupação
@@ -18,7 +23,12 @@ export default function Services() {
                     </p>
                 </div>
                 <div className={styles.serviceCard}>
-                    <Image src={consultancyServiceImage} width={91} height={92} alt="" />
+                    <Image
+                        src={consultancyServiceImage}
+                        width={91}
+                        height={92}
+                        alt="Pessoa com um headset, ao lado de uma ilustração com uma engrenagem e uma chave fixa, representando o serviço de consultoria."
+                    />
                     <h3>Consultoria</h3>
                     <p>
                         Para quem possui um site ou sistema e precisa de ajuda. Ou para quem está a procura de uma
