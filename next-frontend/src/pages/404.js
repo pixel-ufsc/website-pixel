aacc0e15dab71abf3e873948bc599d3f92330513
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-05-13 22:22:52 -0300

    started 404 page

---
+++ next-frontend/src/pages/404.js
@@ -0,0 +1,21 @@
+import PageContainer from '@components/layout/page-container/page-container.component';
+import styles from './../styles/404.module.css';
+import Link from 'next/link';
+
+export default function Custom404() {
+    return (
+        <PageContainer hasFooter={false}>
+            <section className={styles.contentWrapper}>
+                <div className={styles.textContainer}>
+                    <span>Erro</span>
+                    <h1>Ops, parece que essa página não existe...</h1>
+                    <p>Desculpe, não conseguimos encontrar a página que você estava procurando.</p>
+                    <p>
+                        Confira se o endereço digitado está correto ou volte para a <Link href="/">homepage</Link>.
+                    </p>
+                </div>
+                <p className={styles.custom404}>404!</p>
+            </section>
+        </PageContainer>
+    );
+}
