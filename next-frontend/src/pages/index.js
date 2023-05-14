fbf0de5ce0a5bdd889914777a00620ce7d71e1a9
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-05-13 22:37:32 -0300

    Modified styling and created 'SectionDivider' component

--- next-frontend/src/pages/index.js
+++ next-frontend/src/pages/index.js
@@ -2,20 +2,23 @@
 import { getAllDirectors, getAllSocialInfo } from '@lib/sanity.client';
 // Components
 import PageContainer from '@components/layout/page-container/page-container.component';
-import UnderMaintenance from '@components/ui/under-maintenance/under-maintenance.component';
 import IntroContent from '@components/page-home/intro-content/intro-content.component';
 import AboutUs from '@components/page-home/about-us/about-us.component';
 import Directors from '@components/page-home/directors-members/directors.component';
 import Services from '@components/page-home/services/services.component';
+import PortfolioPreview from '@components/page-home/portfolio-preview/portfolio-preview.component';
+import SectionDivider from '@components/ui/section-divider/section-divider.component';
 
 export default function Home({ directorsData }) {
     return (
         <PageContainer>
             <IntroContent />
             <AboutUs />
+            <SectionDivider />
+            <PortfolioPreview />
+            <SectionDivider />
             <Directors directors={directorsData} />
             <Services />
-            <UnderMaintenance />
         </PageContainer>
     );
 }
