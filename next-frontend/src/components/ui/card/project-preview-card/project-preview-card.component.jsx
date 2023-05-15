6a0370aa417d2af683588d1d6510356c0d30b17a
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-05-15 12:01:00 -0300

    Closes #35; Fixed 'ProjectPreview' link grab

--- next-frontend/src/components/ui/card/project-preview-card/project-preview-card.component.jsx
+++ next-frontend/src/components/ui/card/project-preview-card/project-preview-card.component.jsx
@@ -6,34 +6,35 @@ import CardFrame from '../card-frame/card-frame.component';
 
 export default function ProjectPreviewCard({ title, tags, imageSrc, href, ...props }) {
     return (
-        <Link href={href ? href : '/'}>
-            <CardFrame className={styles.container} {...props}>
-                <div className={styles.image_wrapper}>
-                    {imageSrc && (
-                        <Image
-                            fill
-                            sizes="(max-width: 400px) 80vw, 400px"
-                            src={imageSrc}
-                            alt={'Imagem de pré visualização do projeto ' + title}
-                            quality={90}
-                            className={styles.image}
-                        />
-                    )}
-                </div>
-                <div className={styles.title_wrapper}>
+        <CardFrame className={styles.container} {...props}>
+            <div className={styles.image_wrapper}>
+                {imageSrc && (
+                    <Image
+                        fill
+                        sizes="(max-width: 400px) 80vw, 400px"
+                        src={imageSrc}
+                        alt={'Imagem de pré visualização do projeto ' + title}
+                        quality={90}
+                        className={styles.image}
+                    />
+                )}
+            </div>
+
+            <div className={styles.title_wrapper}>
+                <Link href={href ? href : '/portfolio'}>
                     <h3>{title}</h3>
-                    {tags && tags.length > 0 && (
-                        <div className={styles.tags_container}>
-                            {tags.map((tag, index) => (
-                                <span key={index}>
-                                    {tag}
-                                    {index != tags.length - 1 ? ' ∙ ' : ''}
-                                </span>
-                            ))}
-                        </div>
-                    )}
-                </div>
-            </CardFrame>
-        </Link>
+                </Link>
+                {tags && tags.length > 0 && (
+                    <div className={styles.tags_container}>
+                        {tags.map((tag, index) => (
+                            <span key={index}>
+                                {tag}
+                                {index != tags.length - 1 ? ' ∙ ' : ''}
+                            </span>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </CardFrame>
     );
 }
