652bce03c8bab5beb6fbe0a8bbc4111b6d60bfb1
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-05-14 22:25:41 -0300

    Added responsivity to 404 page

--- next-frontend/src/pages/404.js
+++ next-frontend/src/pages/404.js
@@ -1,21 +1,31 @@
-import PageContainer from '@components/layout/page-container/page-container.component';
 import styles from './../styles/404.module.css';
 import Link from 'next/link';
+// Components
+import PageContainer from '@components/layout/page-container/page-container.component';
+import SimpleTitle from '@components/ui/title/simple-title/simple-title.component';
 
 export default function Custom404() {
     return (
-        <PageContainer>
-            <section className={styles.contentWrapper}>
-                <div className={styles.textContainer}>
-                    <span>Erro</span>
-                    <h1>Ops, parece que essa página não existe...</h1>
-                    <p>Desculpe, não conseguimos encontrar a página que você estava procurando.</p>
+        <PageContainer hasWhatsappButton={false}>
+            <div className={styles.container}>
+                <div className={styles.text_container}>
+                    <span className={styles.tag}>Erro</span>
+                    <SimpleTitle className={styles.title} textColor={'purple-gradient'}>
+                        Ops, parece que essa página não existe...
+                    </SimpleTitle>
                     <p>
-                        Confira se o endereço digitado está correto ou volte para a <Link href="/">homepage</Link>.
+                        Desculpe, não conseguimos encontrar a página que você estava procurando. Confira se o endereço
+                        digitado está correto ou volte para a <Link href={'/'}>home</Link>.
                     </p>
                 </div>
-                <p className={styles.custom404}>404!</p>
-            </section>
+
+                <div className={styles.gradient_wrapper}>
+                    <h1>404!</h1>
+                </div>
+            </div>
+            <div className={styles.icon_wrapper}>
+                <div id={styles.pixel_icon}></div>
+            </div>
         </PageContainer>
     );
 }
