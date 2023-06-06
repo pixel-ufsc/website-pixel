d730869c88d8f48a3a0d7143ccc62fdff294a14e
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-06-06 15:34:44 -0300

    finished portifolio page

--- next-frontend/src/components/ui/card/project-card/project-card.component.jsx
+++ next-frontend/src/components/ui/card/project-card/project-card.component.jsx
@@ -1,3 +1,40 @@
-import styles from './project-card.module.scss';
+import styles from './project-card.module.css';
+import Link from 'next/link';
+import CardFrame from '../card-frame/card-frame.component';
+import Image from 'next/image';
 
-const ProjectCard = ({ project }) => {};
+export default function ProjectCard({ title, tags, imageSrc, href, ...props }) {
+    return (
+        <CardFrame className={styles.container} {...props}>
+            <div className={styles.image_wrapper}>
+                {imageSrc && (
+                    <Image
+                        fill
+                        sizes="(max-width: 640px) 80vw, 640px"
+                        src={imageSrc}
+                        alt={'Imagem do projeto ' + title}
+                        quality={90}
+                        className={styles.image}
+                    />
+                )}
+            </div>
+
+            <div className={styles.title_wrapper}>
+                <Link href={href}>
+                    <h3>{title}</h3>
+                </Link>
+                <p>Possibilita que as informações sejam divulgadas.</p>
+                {tags && tags.length > 0 && (
+                    <div className={styles.tags_container}>
+                        {tags.map((tag, index) => (
+                            <span key={index}>
+                                {tag}
+                                {index != tags.length - 1 ? ' ∙ ' : ''}
+                            </span>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </CardFrame>
+    );
+}
