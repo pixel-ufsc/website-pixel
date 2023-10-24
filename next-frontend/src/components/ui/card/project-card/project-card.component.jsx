e4fa0a29f6c25a370fc45d84bd37667fc6652c6c
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-10-24 17:40:03 -0300

    Closes #50; Fixed card tag overflow on mobile

--- next-frontend/src/components/ui/card/project-card/project-card.component.jsx
+++ next-frontend/src/components/ui/card/project-card/project-card.component.jsx
@@ -5,7 +5,12 @@ import Link from 'next/link';
 import CardFrame from '@components/ui/card/card-frame/card-frame.component';
 import SpanTag from '@components/ui/tag/span-tag/span-tag.component';
 
-export default function ProjectCard({ title, tags, imageSrc, href, description, ...props }) {
+export default function ProjectCard({ title, dateFinished, tags, imageSrc, href, description, ...props }) {
+    // From the date received from sanity (e.g. '2023-07-29'), extract the year (e.g. '2023')
+    function getYearFromDate(date) {
+        return date.split('-')[0];
+    }
+
     return (
         <CardFrame className={styles.container} {...props}>
             {/* TODO - REMOVE TARGET ATRIBUTE FOR LINK */}
@@ -24,12 +29,18 @@ export default function ProjectCard({ title, tags, imageSrc, href, description, 
                 </div>
 
                 <div className={styles.text_content_wrapper}>
-                    <h3>{title}</h3>
+                    <h3>
+                        {title}
+                        {dateFinished && <span>{getYearFromDate(dateFinished)}</span>}
+                    </h3>
                     {description && <p>{description}</p>}
                     {tags && tags.length > 0 && (
                         <div className={styles.tags_container}>
                             {tags.map((tag, index) => (
-                                <SpanTag key={index}>{tag}</SpanTag>
+                                <span key={index}>
+                                    {tag}
+                                    {index != tags.length - 1 ? ' ∙ ' : ''}
+                                </span>
                             ))}
                         </div>
                     )}
