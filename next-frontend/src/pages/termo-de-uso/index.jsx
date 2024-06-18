220d28ae5608c9fc9d3241783ccb5b95b120165b
medeirosJose <62633628+medeirosJose@users.noreply.github.com>
2024-06-18 01:39:59 -0300

    hotfix(termos-de-uso): updating terms of use

--- next-frontend/src/pages/termo-de-uso/index.jsx
+++ next-frontend/src/pages/termo-de-uso/index.jsx
@@ -8,56 +8,45 @@ export default function TermoDeUso() {
         <PageContainer>
             <ArticleContainer className={styles.contentWrapper}>
                 <article className={styles.topic}>
-                    <h2>1. Introdução: o que é e pra que serve um termo de uso?</h2>
+                    <h2>1. O que são e para que servem os Termos de Uso?</h2>
                     <p>
-                        O termo de uso é um documento elaborado para obter consentimento dos usuários sobre condições de
-                        navegação e utilização de diversos recursos do nosso site. Em sua redação, trazemos informações
-                        pertinentes ao uso e tratamento de dados pessoais, sempre em conformidade com a Lei Geral de
-                        Proteção de Dados Pessoais (Lei nº 13.709).
-                    </p>
-                    <p>
-                        Queremos garantir aos nossos usuários segurança jurídica desde o início. Portanto, recomendamos
-                        também a leitura de nossa Política de Privacidade (inserir link). Enquanto o termo de uso versa
-                        mais sobre o uso geral do site, a política de privacidade regula e detalha como se dará todo o
-                        tratamento da Pixel sobre os dados fornecidos a nós, além de fornecer a finalidade de cada dado
-                        solicitado.
+                        Os Termos de Uso consistem em um documento elaborado para funcionar como um “contrato” entre um
+                        usuário, seja pessoa física ou jurídica, e a plataforma. No caso, os Termos de Uso aqui
+                        descritos estabelecem uma relação obrigacional entre a Pixel e os usuários do nosso site.
                     </p>
                     <p>
                         É importante salientar que todas as obrigações aqui descritas deverão ser aceitas em sua
-                        plenitude por livre e espontânea vontade, podendo o usuário concordar ou não com elas. Será
-                        considerado que o termo foi consentido caso o usuário se cadastre ou forneça algum dado em nosso
-                        site. Não estará autorizado a se cadastrar aquele que não concordar com as disposições aqui
-                        descritas. Além disso, esses termos se aplicam tanto a pessoas naturais quanto a pessoas
-                        jurídicas.
+                        plenitude, por livre e espontânea vontade, podendo o usuário concordar com elas ou não.
+                        Considera-se que houve aceite/concordância caso o usuário realize seu cadastro ou forneça algum
+                        dado em nossa plataforma. Aquele que não concordar com as disposições aqui previstas não está
+                        autorizado a se cadastrar.
                     </p>
                     <p>
-                        Sendo assim, o presente termo estabelece uma relação obrigacional entre a Pixel e os usuários do
-                        nosso site. Uma vez consentida, o usuário não poderá alegar desconhecimento ou a não ciência das
-                        condições previstas neste termo ou em nossa Política de Privacidade.
+                        Uma vez registrado o aceite/concordância, o usuário não poderá alegar desconhecimento das
+                        condições previstas nestes Termos de Uso.
+                    </p>
+                    <p>
+                        A fim de garantir ainda mais segurança aos nossos usuários, recomendamos também a leitura de
+                        nossa Política de Privacidade (inserir link), a qual regula e detalha como se dá o tratamento da
+                        Pixel sobre os dados a nós fornecidos.
                     </p>
                 </article>
                 <article className={styles.topic}>
                     <h2>2. Cadastro e funcionalidades do site</h2>
                     <p>
-                        Para obter acesso ao nosso canal de comunicação, qual seja o campo “Contato” ou “Fazer
-                        orçamento”, é exigido o preenchimento de um formulário que estão presentes nas abas da página
-                        inicial do site.
+                        Para obter acesso ao nosso canal de comunicação, pelos campos <i>“Contato”</i> ou{' '}
+                        <i>“Fazer orçamento”</i>, exigimos o preenchimento de um formulário que consta nas abas da
+                        página inicial do site. O usuário que utilizar este recurso se obriga a respeitar os Termos de
+                        Uso, informando dados verídicos e completos. Além disso, o usuário declara ser plenamente capaz
+                        ou, caso não seja, que possui um representante para usufruir da plataforma.
                     </p>
                     <p>
-                        O usuário que fizer a utilização deste recurso, obriga-se a observar e respeitar os Termos e
-                        Condições de Uso, informando dados completos e válidos, sempre verídicos. Além disso, o usuário
-                        declara ser plenamente capaz, caso não seja, que possua um representante para exercer e usufruir
-                        da plataforma.
+                        Após o envio do formulário, a equipe comercial poderá entrar em contato para dar continuidade ao
+                        atendimento ao usuário/cliente.
                     </p>
                     <p>
-                        Os dados enviados pelo interessado, bem como aqueles fornecidos durante a execução do serviço,
-                        serão tratados pela Pixel, de acordo com o elencado em nossa Política de Privacidade. Dessa
-                        forma, após o envio do formulário, a equipe de comercial poderá entrar em contato para dar
-                        continuidade ao atendimento ao usuário/cliente.
-                    </p>
-                    <p>
-                        Para a realização do contato será necessário seguir o seguinte passo a passo na aba “Contato” ou
-                        “Fazer orçamento”:
+                        Para que seja realizado esse primeiro contato será necessário seguir o seguinte passo a passo,
+                        que se aplica tanto às abas <i>“Contato”</i> e <i>“Fazer orçamento”</i>:
                     </p>
                     <ol>
                         <li>Inserir seu nome;</li>
@@ -67,38 +56,58 @@ export default function TermoDeUso() {
                         <li>Clicar no botão "Enviar E-mail".</li>
                     </ol>
                     <p>
-                        Também poderá ser possível entrar em contato diretamente através do WhatsApp da Pixel por meio
-                        de um clique no símbolo do aplicativo que se encontra na tela inicial do site.
+                        Também é possível entrar em contato com a Empresa Júnior diretamente pelo WhatsApp, por meio de
+                        um clique no símbolo do aplicativo que se encontra na tela inicial do site.
                     </p>
                     <p>
-                        Assim que nossa equipe receber a sua mensagem, entraremos em contato através do e-mail fornecido
-                        para analisar a possibilidade de uma reunião de diagnóstico e, posteriormente, a elaboração e
-                        apresentação de proposta.
+                        É importante destacar que todos os dados fornecidos pelo usuário serão tratados pela Pixel de
+                        acordo com as previsões de nossa <a href="/politica-de-privacidade">Política de Privacidade</a>.
                     </p>
                     <p>
-                        Acerca das funcionalidades presentes em nosso site, na tela inicial se encontra informações
-                        sobre nossos serviços, sobre a empresa, nosso blog, nosso portfólio, newsletter e durante a
-                        abertura do processo seletivo, poderá surgir esporadicamente a opção de inscrição através do
-                        site.
+                        Assim que nossa equipe receber a sua mensagem (seja por mensagem, seja pelo site), entraremos em
+                        contato através do e-mail fornecido para analisar a possibilidade de uma reunião de diagnóstico
+                        e, posteriormente, elaboração e apresentação de proposta.
+                    </p>
+                    <p>
+                        Acerca das funcionalidades presentes em nosso site, na tela inicial você encontra informações
+                        sobre nossos serviços, sobre a empresa, bem como nosso blog, portfólio e <i>newsletter</i>.
+                        Durante a abertura do processo seletivo para integrar a Empresa Júnior, poderá surgir também a
+                        opção de inscrição por meio do site.
                     </p>
                 </article>
                 <article className={styles.topic}>
-                    <h2>3. Exoneração de responsabilidade</h2>
+                    <h2>3. Responsabilidades</h2>
+                    <h3>3.1 Uso da plataforma e fornecimento de informações</h3>
                     <p>
-                        A Pixel não mede esforços para informar, proteger e atender seus clientes, porém estes são os
-                        únicos responsáveis pela correta utilização das ferramentas do nosso site e veracidade das
-                        informações nele expostas. Dessa forma, não nos responsabilizamos por qualquer prejuízo ou dano
-                        que os usuários venham a ter relacionados à má utilização da ferramenta. A Pixel se exime
-                        expressamente de:
+                        Os usuários/clientes da Pixel serão os únicos responsáveis pela correta utilização das
+                        ferramentas do nosso site e pela veracidade das informações nele expostas. Nesse sentido, a
+                        Empresa Júnior não se responsabiliza por qualquer dano ou prejuízo que o usuário venha a ter em
+                        razão da má utilização da ferramenta.
                     </p>
-                    <ol>
-                        <li>Envio de informações incorretas no formulário;</li>
-                        <li>Acesso ao site da Pixel por meio de navegadores não confiáveis;</li>
-                        <li>Impossibilidade do funcionamento de alguma função do site.</li>
-                    </ol>
+                    <p>
+                        Além disso, a Pixel se exime expressamente de responsabilidade nos casos de envio de informações
+                        incorretas via formulário, acesso ao site por meio de navegadores não confiáveis e
+                        impossibilidade de execução de alguma funcionalidade do site.
+                    </p>
+                    <h3>3.2 Conteúdos publicados</h3>
+                    <p>
+                        Todo o conteúdo exposto neste site, independentemente de autoria, é de responsabilidade da
+                        Pixel, como instituição, devendo ser direcionada a ela qualquer tipo de solicitação e/ou
+                        reclamação relacionada ao material publicado.
+                    </p>
+                    <p>
+                        A Pixel também não se responsabiliza por nenhum dano ou prejuízo gerado pela má interpretação
+                        dos conteúdos publicados em seu blog, bem como a desconformidade, imprecisão ou desatualização
+                        das informações ali expostas.
+                    </p>
                 </article>
                 <article className={styles.topic}>
                     <h2>4. Canais de comunicação</h2>
+                    <p>
+                        Ao concordar com estes Termos de Uso, o usuário autoriza a abertura de canais de comunicação por
+                        parte da Pixel, inclusive pelas redes sociais, a fim de facilitar o diálogo a respeito da nossa
+                        área de atuação e serviços buscados pelo usuário.
+                    </p>
                     <p>
                         A Pixel leva em média 3 (três) dias corridos, contados a partir da data de envio do formulário
                         de contato, para responder às mensagens que são enviadas ao nosso e-mail:{' '}
@@ -109,47 +118,34 @@ export default function TermoDeUso() {
                         <a href="https://wa.me/message/PB4N2GQBAAI4N1" target="_blank">
                             +55 48 9209-2256
                         </a>
-                        . Desta feita, o usuário autoriza, pelos mais variados meios, incluindo redes sociais, mas não
-                        se limitando a elas, a abertura de canais de comunicação por parte da Pixel, a fim de
-                        proporcionar o diálogo a respeito da nossa área de atuação e serviços almejados pelo usuário.
+                        .
                     </p>
                 </article>
                 <article className={styles.topic}>
-                    <h2>5. Propriedade intelectual</h2>
+                    <h2>6. Alterações nestes Termos de Uso</h2>
                     <p>
-                        A Pixel preza pelo respeito à propriedade intelectual de terceiros e espera que seus usuários
-                        façam o mesmo. Portanto, é importante ressaltar a conformidade do presente termo com o Marco
-                        Civil da Internet (Lei n° 12.965/2014) e com a Lei de Direitos Autorais (Lei Federal nº
-                        8.906/1994). Assim sendo, fica estabelecido que todo conteúdo produzido nesse site,
-                        independentemente de autoria, é de responsabilidade da Pixel como instituição, devendo ser
-                        direcionada a ela qualquer tipo de obrigação, mérito, reconhecimento ou consequência proveniente
-                        de qualquer produção.
-                    </p>
-                    <p>
-                        Além disso, a Pixel não se responsabiliza por nenhum dano ou prejuízo gerado pela má
-                        interpretação dos conteúdos publicados em seu blog, bem como a desconformidade, imprecisão ou
-                        desatualização das informações ali expostas.
+                        A Pixel se reserva ao direito de modificar e/ou atualizar seus Termos de Uso sempre que
+                        considerar necessário, razão pela qual aconselhamos que esse documento seja lido/conferido
+                        periodicamente.
                     </p>
                 </article>
                 <article className={styles.topic}>
-                    <h2>6. Alterações nesta política</h2>
+                    <h2>7. Outras disposições importantes</h2>
                     <p>
-                        A Pixel se reserva ao direito de modificar e/ou atualizar seus termos sempre que considerar
-                        necessário, portanto, aconselhamos que esse documento seja lido sempre que possível. Fica eleito
-                        desde já o foro da comarca de Florianópolis/SC para solucionar eventuais omissões ou
-                        controvérsias oriundas deste termo. Depois que obtido o consentimento do usuário, esse documento
-                        terá efeito imediato.
+                        Fica eleito desde já o foro da Comarca de Florianópolis/SC para solucionar eventuais omissões ou
+                        controvérsias oriundas destes Termos de Uso.
                     </p>
                     <p>
-                        Por fim,{' '}
-                        <strong>
-                            o usuário declara ter lido atentamente e compreendido os termos e condições aqui dispostos,
-                            além de estar ciente de todo o seu teor, expressando seu consentimento de maneira livre,
-                            informada, inequívoca e expressa.
-                        </strong>
+                        Depois de obtida a ciência/concordância do usuário (tópico 1), esse documento terá efeito
+                        imediato.
+                    </p>
+                    <p>
+                        Por fim, o usuário declara ter lido atentamente e compreendido os termos e condições aqui
+                        dispostos, além de estar ciente de todo o seu teor, expressando seu consentimento de maneira
+                        livre, informada, expressa e inequívoca.
                     </p>
                 </article>
-                <span className={styles.lastModification}>Última alteração: 17/12/2021</span>
+                <span className={styles.lastModification}>Última alteração: 18/06/2024</span>
             </ArticleContainer>
         </PageContainer>
     );
