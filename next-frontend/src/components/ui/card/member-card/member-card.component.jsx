aaa75b7f74ddaa8370667fe506f18940cb2cabfa
Jose Eduardo <joseeduardomj@gmail.com>
2025-01-22 13:38:18 -0300

    fix: role for former members is now optional

--- next-frontend/src/components/ui/card/member-card/member-card.component.jsx
+++ next-frontend/src/components/ui/card/member-card/member-card.component.jsx
@@ -2,7 +2,7 @@ import CardFrame from '@components/ui/card/card-frame/card-frame.component';
 import styles from './member-card.module.css';
 import Image from 'next/image';
 
-export default function MemberCard({ imageSrc, name, role, ...imageProps }) {
+export default function MemberCard({ imageSrc, name, role = '', ...imageProps }) {
     return (
         <CardFrame borderRadius={'6%'} className={styles.container}>
             <div className={styles.image_wrapper}>
@@ -20,8 +20,9 @@ export default function MemberCard({ imageSrc, name, role, ...imageProps }) {
             </div>
             <div className={styles.text_container}>
                 <h4>{name ? name : 'Name'}</h4>
-                <h5>{role ? role : 'Role'}</h5>
+                <h5>{role ? role : ''}</h5>
             </div>
         </CardFrame>
     );
 }
+
